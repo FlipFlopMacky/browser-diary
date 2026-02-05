403c51c0ec711ea6a6bf05bd602613177a879b55
Fumiya Makino <flipflop.macky.github@gmail.com>
2026-02-05 13:44:44 +0900

    日記アプリベース

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+import { defineNuxtConfig } from 'nuxt/config'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2026-02-05',
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
+})
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen bg-gray-50 p-8">
+    <div class="max-w-4xl mx-auto">
+      <h1 class="text-4xl font-bold text-gray-900 mb-8">ブラウザ日記</h1>
+      <p class="text-gray-600">日記アプリへようこそ</p>
+    </div>
+  </div>
+</template>
+
